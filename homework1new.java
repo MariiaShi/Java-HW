ac938e93e4353bf07b7dfbe771e6599b3eb77d1e
Masha <165020790+MariiaShi@users.noreply.github.com>
2024-08-04 12:58:17 +0200

    new1

---
+++ homework1new.java
@@ -0,0 +1,44 @@
+package org.example.homeworks.homework1;
+
+public class homework1new {
+    public static void main(String[] args) {
+        char charValue = 'G';
+        int intValue = 89;
+        byte byteValue = 4;
+        short shortValue = 56;
+        float floatValue = 4.7333436f;
+        double doubleValue = 4.355453532;
+        long longValue = 12121L;
+
+
+        Character characterValue = 'G';
+        Integer integerValue = 89;
+        Byte byteObjectValue = 4;
+        Short shortObjectValue = 56;
+        Float floatObjectValue = 4.7333436f;
+        Double doubleObjectValue = 4.355453532;
+        Long longObjectValue = 12121L;
+
+
+        System.out.println("char: " + charValue);
+        System.out.println("int: " + intValue);
+        System.out.println("byte: " + byteValue);
+        System.out.println("short: " + shortValue);
+        System.out.println("float: " + floatValue);
+        System.out.println("double: " + doubleValue);
+        System.out.println("long: " + longValue);
+
+        System.out.println();
+
+        System.out.println("Character: " + characterValue);
+        System.out.println("Integer: " + integerValue);
+        System.out.println("Byte: " + byteObjectValue);
+        System.out.println("Short: " + shortObjectValue);
+        System.out.println("Float: " + floatObjectValue);
+        System.out.println("Double: " + doubleObjectValue);
+        System.out.println("Long: " + longObjectValue);
+
+    }
+}
+
+
